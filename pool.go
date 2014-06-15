d3d864b0ae665b93caafdcdd12f60b6c01a0e906
¨Huu Khiem (Mark)¨ <tonghuukhiem@gmail.com>
2014-06-15 08:46:46 +0800

    docs

--- pool.go
+++ pool.go
@@ -1,3 +1,10 @@
+/*
+Create a pool of fixed-length byte-slices shared between go routines
+By caching unused slices, it reduce garbage-collector work
+hence faster programs
+
+This is similar to sync.Pool in (unreleased) Go 1.3
+*/
 package bytepool
 
 // Package bytepool provides a pool of fixed-length []byte
@@ -6,6 +13,10 @@ import (
 	"sync/atomic"
 )
 
+// The pool of byte-slices
+//    misses: count when checkout fails (there's no more slices)
+//    capacity: size of each slices 
+//    list: the pool
 type Pool struct {
 	misses   int32
 	capacity int
@@ -23,6 +34,9 @@ func New(count int, capacity int) *Pool {
 	return p
 }
 
+// Get an item out from the pool
+// when there are not enough slices available, it blocks
+// and also increase the misses count
 func (pool *Pool) Checkout() *Item {
 	var item *Item
 	select {
@@ -34,6 +48,7 @@ func (pool *Pool) Checkout() *Item {
 	return item
 }
 
+// no of items left inside the pool
 func (pool *Pool) Len() int {
 	return len(pool.list)
 }
